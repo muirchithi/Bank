b5aab84eb9ee621e61cfd8013bf5590a507701c8
muirchithi <DennisKlug1@googlemail.com>
2024-01-31 21:39:15 +0100

    Integration of the spring framework
    Setup configurations for the spring framework, updating necessary dependencies

---
+++ src/main/java/com/bank/MyApplication.java
@@ -0,0 +1,13 @@
+package com.bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+public class MyApplication {
+
+
+    public static void main(String[] args){
+
+        SpringApplication.run(MyApplication.class, args);
+    }
+}
---
+++ src/main/java/com/bank/ui/Ui.java
@@ -0,0 +1,33 @@
+package com.bank.ui;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Ui extends Application {
+
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        StackPane root = new StackPane();
+
+        Button btn = new Button("click me");
+
+        root.getChildren().add(btn);
+
+        Scene scene = new Scene(root, 900, 650);
+
+        primaryStage.setScene(scene);
+
+        primaryStage.setTitle("My App");
+
+        primaryStage.show();
+    }
+
+    public static void main(String[] args){
+        launch(args);
+    }
+}
