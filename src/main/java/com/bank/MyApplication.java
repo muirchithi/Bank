888ecf531372fb79663a3867a002774ed31fb4a5
muirchithi <DennisKlug1@googlemail.com>
2024-01-31 23:26:43 +0100

    Login Interface Implementation
    Implementation of a basic interface to improve on

--- src/main/java/com/bank/MyApplication.java
+++ src/main/java/com/bank/MyApplication.java
@@ -1,13 +1,21 @@
 package com.bank;
 
+import com.bank.ui.LoginUi;
+import javafx.application.Application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class MyApplication {
 
+        private static String[] savedArgs;
 
     public static void main(String[] args){
+        savedArgs = args;
+        Application.launch(LoginUi.class,args);
+    }
 
-        SpringApplication.run(MyApplication.class, args);
+    public static void initSpring(){
+        SpringApplication.run(MyApplication.class, savedArgs);
     }
 }
